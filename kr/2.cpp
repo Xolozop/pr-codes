092c8e9886713c29016a1f398a19bc9ecac55b79
Xolozop <50333438+Xolozop@users.noreply.github.com>
2024-04-12 17:14:25 +0300

    Update 2.cpp

--- kr/2.cpp
+++ kr/2.cpp
@@ -14,50 +14,50 @@ public:
 
 public:
 	Queue(){
-        this->head = NULL;
-        this->tail = NULL;
-        this->count = 0;
+	this->head = NULL;
+	this->tail = NULL;
+	this->count = 0;
 	}
 
-    ~Queue(){
-        while (this->count > 0)
-            this->pop();
-    }
+	~Queue(){
+	while (this->count > 0)
+	    this->pop();
+	}
 
 	void pushHead(int val) { // кладет элемент в очередь (в голову списка)
-        ListElem* nw = (ListElem*)malloc(sizeof(ListElem));
-        nw->next = NULL;
-        nw->prev = NULL;
-        nw->val = val;
-        if (this->count == 0) {
-            this->head = nw;
-            this->tail = nw;
-        } else {
-            nw->next = this->head;
-            this->head->prev = nw;
-            this->head = nw;
-        }
-        count++;
+		ListElem* nw = (ListElem*)malloc(sizeof(ListElem));
+		nw->next = NULL;
+		nw->prev = NULL;
+		nw->val = val;
+		if (this->count == 0) {
+		    this->head = nw;
+		    this->tail = nw;
+		} else {
+		    nw->next = this->head;
+		    this->head->prev = nw;
+		    this->head = nw;
+		}
+		count++;
 	}
 
 	void pop(){ 
-        if (this->count > 0) {
-            ListElem* del = this->tail;
-            this->tail = this->tail->prev;
-            free(del);
-            count--;
-        }
+	        if (this->count > 0) {
+	            ListElem* del = this->tail;
+	            this->tail = this->tail->prev;
+	            free(del);
+	            count--;
+	        }
 	}
 
 	bool isEmpty(){ 
-        return (count == 0) ? true : false;
+        	return (count == 0) ? true : false;
 	}
 
 	int getHead(){ 
-        return this->head ? this->head->val : 0;
+        	return this->head ? this->head->val : 0;
 	}
 
 	int getTail(){ 
-        return this->tail ? this->tail->val : 0;
+        	return this->tail ? this->tail->val : 0;
 	}
 };
